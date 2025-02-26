94d4e81c2c2f9cc961574ddf42044329fbcb772b
Raman Gupta <gr.gupta.raman@gmail.com>
2025-02-25 21:02:14 -0500

    feat: remove unnecessary unique constraints

---
+++ drizzle/0003_perfect_makkari.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "course_components" DROP CONSTRAINT "course_components_courseCode_unique";--> statement-breakpoint
+ALTER TABLE "course_components" DROP CONSTRAINT "course_components_term_unique";--> statement-breakpoint
+ALTER TABLE "course_components" DROP CONSTRAINT "course_components_subSection_unique";
